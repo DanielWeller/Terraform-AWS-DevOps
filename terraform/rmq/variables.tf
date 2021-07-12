1d74d75d818fef478d550cdc7ca9bab85d66752a
Daniel Weller <daniel.weller-mes@gmx.de>
2021-07-12 17:41:05 +0200

    inital commit - terraform

---
+++ terraform/rmq/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+    description = "Name the instance to deploy"
+}
+
+variable "group" {
+    description = "the group tag for ansible to identify RMQ"
+}
+
+variable "profile" {
+    description = "profile we will use for deploy"
+}
